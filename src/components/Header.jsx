e2a9c83b2989f1dc4b4ac2f2135a116620dff4a9
StreamGearVN <141522098+devinits2@users.noreply.github.com>
2026-08-29 15:56:42 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from "../assets/logo.png";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} alt="A canvas" />
+      <h1>ReactArt</h1>
+      <p>A community of artists and art-lovers.</p>
+      <p>hello world</p>
+    </header>
+  );
+}
